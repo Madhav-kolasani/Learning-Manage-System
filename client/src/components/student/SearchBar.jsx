de753eb9975f9d601403afa53e7a583c69fec886
Madhav <madhavkolasani@gmail.com>
2025-09-05 11:33:04 +0530

    SearchBar

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const SearchBar = () => {
+const SearchBar = ({ data }) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : "");
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
+
   return (
-    <div><h1>SearchBar</h1></div>
-  )
-}
+    <div className="w-full flex justify-center">
+      <form
+        onSubmit={onSearchHandler}
+        className="w-full max-w-2xl flex items-center bg-white border border-gray-300 rounded-full shadow-md overflow-hidden"
+      >
+        {/* Search Icon */}
+        <div className="flex items-center justify-center px-4">
+          <img
+            src={assets.search_icon}
+            alt="search_icon"
+            className="w-6 h-6 opacity-70"
+          />
+        </div>
 
-export default SearchBar
+        {/* Input */}
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          placeholder="Search for courses"
+          className="flex-grow h-14 px-2 text-gray-700 text-lg outline-none"
+        />
+
+        {/* Button */}
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 transition text-white font-medium px-8 h-14 rounded-r-full"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
