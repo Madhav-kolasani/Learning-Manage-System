10b76764fa1a86fd8c85dbee1b8f09756ce735de
Madhav <madhavkolasani@gmail.com>
2025-09-06 18:00:50 +0530

    Customised Rating

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70 to-white">
+    <div className="flex flex-col items-center justify-center w-full md:pt-13 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70 to-white">
       <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl max-w-auto">
         Empower your future with the courses designed{" "}
         <span className="text-blue-600">to fit your choice.</span>
