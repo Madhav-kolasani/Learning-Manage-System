2fa503569faed5abca6673bf3217e8a73261eec6
Madhav <madhavkolasani@gmail.com>
2025-09-07 11:29:42 +0530

    educator dashboard and sidebar

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-Outlet
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
+import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
+Outlet;
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator</h1>
-      <div><Outlet /></div>
-    </div>
-  )
-}
+    <>
+      <div className="text-[15px] leading-[21px] min-h-screen bg-white">
+        <Navbar />
+        <div className="flex ">
+          <Sidebar />
+          <div className="flex-1">{<Outlet />}</div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Educator
+export default Educator;
